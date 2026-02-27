956fbd336e2c91b681256dbf2f8d11976cd368ed
azizbek12d-crypto <azizbek12d@gmail.com>
2026-02-27 09:52:04 +0600

    test

---
+++ main.py
@@ -0,0 +1,7 @@
+def show_calendar():
+    print("\n --- Calendar --- ")
+    print("today: 2024-06-01")
+    print("events: register the lab work")
+
+if __name__ == "__main__":
+    show_calendar()
